fed06d41c2b26e10832f95fa65ffba9b841d91ad
Pocol <project.asura@gmail.com>
2020-04-29 15:06:50 +0900

    頂点バッファとインデックスバッファを追加。

---
+++ src/asdxIndexBuffer.cpp
@@ -0,0 +1,130 @@
+﻿//-----------------------------------------------------------------------------
+// File : asdxIndexBuffer.cpp
+// Desc : Index Buffer Wrapper.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <asdxIndexBuffer.h>
+#include <asdxLogger.h>
+
+
+namespace asdx {
+
+///////////////////////////////////////////////////////////////////////////////
+// IndexBuffer class
+///////////////////////////////////////////////////////////////////////////////
+
+//-----------------------------------------------------------------------------
+//      コンストラクタです.
+//-----------------------------------------------------------------------------
+IndexBuffer::IndexBuffer()
+{ memset(&m_View, 0, sizeof(m_View)); }
+
+//-----------------------------------------------------------------------------
+//      デストラクタです.
+//-----------------------------------------------------------------------------
+IndexBuffer::~IndexBuffer()
+{ Term(); }
+
+//-----------------------------------------------------------------------------
+//      初期化処理を行います.
+//-----------------------------------------------------------------------------
+bool IndexBuffer::Init(ID3D12Device* pDevice, uint64_t size)
+{
+    if (pDevice == nullptr || size == 0)
+    {
+        ELOG("Error : Invalid Argument.");
+        return false;
+    }
+
+    D3D12_HEAP_PROPERTIES prop = {};
+    prop.Type                   = D3D12_HEAP_TYPE_UPLOAD;
+    prop.CPUPageProperty        = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+    prop.MemoryPoolPreference   = D3D12_MEMORY_POOL_UNKNOWN;
+    prop.VisibleNodeMask        = 1;
+    prop.CreationNodeMask       = 1;
+
+    D3D12_RESOURCE_DESC desc = {};
+    desc.Dimension          = D3D12_RESOURCE_DIMENSION_BUFFER;
+    desc.Width              = size;
+    desc.Height             = 1;
+    desc.DepthOrArraySize   = 1;
+    desc.Format             = DXGI_FORMAT_UNKNOWN;
+    desc.MipLevels          = 1;
+    desc.SampleDesc.Count   = 1;
+    desc.SampleDesc.Quality = 0;
+    desc.Layout             = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+    desc.Flags              = D3D12_RESOURCE_FLAG_NONE;
+
+    auto state = D3D12_RESOURCE_STATE_GENERIC_READ;
+    auto flags = D3D12_HEAP_FLAG_NONE;
+
+    auto hr = pDevice->CreateCommittedResource(
+        &prop,
+        flags,
+        &desc,
+        state,
+        nullptr,
+        IID_PPV_ARGS(m_pResource.GetAddress()));
+    if ( FAILED(hr) )
+    { return false; }
+
+    m_View.BufferLocation   = m_pResource->GetGPUVirtualAddress();
+    m_View.SizeInBytes      = size;
+    m_View.Format           = DXGI_FORMAT_R32_UINT;
+
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+//      終了処理を行います.
+//-----------------------------------------------------------------------------
+void IndexBuffer::Term()
+{
+    m_pResource.Reset();
+    memset(&m_View, 0, sizeof(m_View));
+}
+
+//-----------------------------------------------------------------------------
+//      メモリマッピングを行います.
+//-----------------------------------------------------------------------------
+void* IndexBuffer::Map()
+{
+    if (m_pResource.GetPtr() == nullptr)
+    { return nullptr; }
+
+    void* ptr = nullptr;
+    auto hr = m_pResource->Map(0, nullptr, &ptr);
+    if (FAILED(hr))
+    { return nullptr; }
+
+    return ptr;
+}
+
+//-----------------------------------------------------------------------------
+//      メモリマッピングを解除します.
+//-----------------------------------------------------------------------------
+void IndexBuffer::Unmap()
+{
+    if (m_pResource.GetPtr() == nullptr)
+    { return; }
+
+    m_pResource->Unmap(0, nullptr);
+}
+
+//-----------------------------------------------------------------------------
+//      インデックスバッファビューを取得します.
+//-----------------------------------------------------------------------------
+D3D12_INDEX_BUFFER_VIEW IndexBuffer::GetView() const
+{ return m_View; }
+
+//-----------------------------------------------------------------------------
+//      リソースを取得します.
+//-----------------------------------------------------------------------------
+ID3D12Resource* IndexBuffer::GetResource() const
+{ return m_pResource.GetPtr(); }
+
+} // namespace asdx
---
+++ src/asdxVertexBuffer.cpp
@@ -0,0 +1,126 @@
+﻿//-----------------------------------------------------------------------------
+// File : asdxVertexBuffer.cpp
+// Desc : Vertex Buffer Wrapper.
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <asdxVertexBuffer.h>
+#include <asdxLogger.h>
+
+
+namespace asdx {
+
+//-----------------------------------------------------------------------------
+//      コンストラクタです.
+//-----------------------------------------------------------------------------
+VertexBuffer::VertexBuffer()
+{ memset(&m_View, 0, sizeof(m_View)); }
+
+//-----------------------------------------------------------------------------
+//      デストラクタです.
+//-----------------------------------------------------------------------------
+VertexBuffer::~VertexBuffer()
+{ Term(); }
+
+//-----------------------------------------------------------------------------
+//      初期化処理を行います.
+//-----------------------------------------------------------------------------
+bool VertexBuffer::Init(ID3D12Device* pDevice, uint64_t size, uint32_t stride)
+{
+    if (pDevice == nullptr || size == 0 || stride == 0)
+    {
+        ELOG("Error : Invalid Argument.");
+        return false;
+    }
+
+    D3D12_HEAP_PROPERTIES prop = {};
+    prop.Type                   = D3D12_HEAP_TYPE_UPLOAD;
+    prop.CPUPageProperty        = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+    prop.MemoryPoolPreference   = D3D12_MEMORY_POOL_UNKNOWN;
+    prop.VisibleNodeMask        = 1;
+    prop.CreationNodeMask       = 1;
+
+    D3D12_RESOURCE_DESC desc = {};
+    desc.Dimension          = D3D12_RESOURCE_DIMENSION_BUFFER;
+    desc.Width              = size;
+    desc.Height             = 1;
+    desc.DepthOrArraySize   = 1;
+    desc.Format             = DXGI_FORMAT_UNKNOWN;
+    desc.MipLevels          = 1;
+    desc.SampleDesc.Count   = 1;
+    desc.SampleDesc.Quality = 0;
+    desc.Layout             = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+    desc.Flags              = D3D12_RESOURCE_FLAG_NONE;
+
+    auto state = D3D12_RESOURCE_STATE_GENERIC_READ;
+    auto flags = D3D12_HEAP_FLAG_NONE;
+
+    auto hr = pDevice->CreateCommittedResource(
+        &prop,
+        flags,
+        &desc,
+        state,
+        nullptr,
+        IID_PPV_ARGS(m_pResource.GetAddress()));
+    if ( FAILED(hr) )
+    { return false; }
+
+    m_View.BufferLocation   = m_pResource->GetGPUVirtualAddress();
+    m_View.SizeInBytes      = size;
+    m_View.StrideInBytes    = stride;
+
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+//      終了処理を行います.
+//-----------------------------------------------------------------------------
+void VertexBuffer::Term()
+{
+    m_pResource.Reset();
+    memset(&m_View, 0, sizeof(m_View));
+}
+
+//-----------------------------------------------------------------------------
+//      メモリマッピングを行います.
+//-----------------------------------------------------------------------------
+void* VertexBuffer::Map()
+{
+    if (m_pResource.GetPtr() == nullptr)
+    { return nullptr; }
+
+    void* ptr = nullptr;
+    auto hr = m_pResource->Map(0, nullptr, &ptr);
+    if (FAILED(hr))
+    { return nullptr; }
+
+    return ptr;
+}
+
+//-----------------------------------------------------------------------------
+//      メモリマッピングを解除します.
+//-----------------------------------------------------------------------------
+void VertexBuffer::Unmap()
+{
+    if (m_pResource.GetPtr() == nullptr)
+    { return; }
+
+    m_pResource->Unmap(0, nullptr);
+}
+
+//-----------------------------------------------------------------------------
+//      頂点バッファビューを取得します.
+//-----------------------------------------------------------------------------
+D3D12_VERTEX_BUFFER_VIEW VertexBuffer::GetView() const
+{ return m_View; }
+
+//-----------------------------------------------------------------------------
+//      リソースを取得します.
+//-----------------------------------------------------------------------------
+ID3D12Resource* VertexBuffer::GetResource() const
+{ return m_pResource.GetPtr(); }
+
+} // namespace asdx
