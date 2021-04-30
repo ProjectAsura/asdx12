5cfc783cd731492ed89de0520f0aa60dc9ed42af
Pocol <project.asura@gmail.com>
2021-04-30 17:02:32 +0900

    Entityを追加。

---
+++ include/fw/asdxEntity.h
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------------
+// File : asdxEntity.h
+// Desc : Entity
+// Copyright(c) Project Asura. All right reserved.
+//-----------------------------------------------------------------------------
+#pragma once
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include <map>
+
+
+namespace asdx {
+
+struct IComponent
+{
+    virtual ~IComponent() {}
+    virtual void Update() {}
+    virtual void Draw  () {}
+};
+
+class Entity
+{
+public:
+    bool        Add     (uint32_t id, IComponent* item);
+    bool        Remove  (uint32_t id);
+    IComponent* Get     (uint32_t id) const;
+    void        Update  ();
+    void        Draw    ();
+
+private:
+    std::map<uint32_t, IComponent*> m_Components;
+};
+
+} // namespace asdx
