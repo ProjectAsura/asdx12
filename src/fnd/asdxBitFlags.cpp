ee967f8b22dead1763f7d0a1d9b0114f85d313e9
Pocol <project.asura@gmail.com>
2024-05-20 17:32:09 +0900

    CountBitを追加。

--- src/fnd/asdxBitFlags.cpp
+++ src/fnd/asdxBitFlags.cpp
@@ -101,7 +101,7 @@ static int CountZeroL(uint64_t value)
     return CountBit64(~value);
 }
 
-static int CountZeroR(uint8_t  value) { return CountBit8((~value) & (value - 1)); }
+static int CountZeroR(uint8_t  value) { return CountBit8 ((~value) & (value - 1)); }
 static int CountZeroR(uint16_t value) { return CountBit16((~value) & (value - 1)); }
 static int CountZeroR(uint32_t value) { return CountBit32((~value) & (value - 1)); }
 static int CountZeroR(uint64_t value) { return CountBit64((~value) & (value - 1)); }
@@ -110,6 +110,7 @@ static int CountZeroR(uint64_t value) { return CountBit64((~value) & (value - 1)
 
 #if !_HAS_CXX20
 
+int CountBit  (uint8_t value) { return impl::CountBit8 (value); }
 int CountZeroL(uint8_t value) { return impl::CountZeroL(value); }
 int CountZeroR(uint8_t value) { return impl::CountZeroR(value); }
 
@@ -117,6 +118,11 @@ int CountZeroR(uint8_t value) { return impl::CountZeroR(value); }
 
 #if _HAS_CXX20
 // C++ 20.
+
+int CountBit(uint8_t  value) { return std::popcount(value); }
+int CountBit(uint16_t value) { return std::popcount(value); }
+int CountBit(uint32_t value) { return std::popcount(value); }
+int CountBit(uint64_t value) { return std::popcount(value); }
 
 int CountZeroL(uint8_t  value) { return std::countl_zero(value); }
 int CountZeroL(uint16_t value) { return std::countl_zero(value); }
@@ -131,6 +137,11 @@ int CountZeroR(uint64_t value) { return std::countr_zero(value); }
 #elif defined(__clang__) || defined(__GNUC__)
 // GCC or clang.
 
+int CountBit(uint8_t  value) { return __builtin_popcount(value); }
+int CountBit(uint16_t value) { return __builtin_popcount(value); }
+int CountBit(uint32_t value) { return __builtin_popcount(value); }
+int CountBit(uint64_t value) { return __builtin_popcountll(value); }
+
 int CountZeroL(uint16_t value) { return __builtin_clzs(value); }
 int CountZeroL(uint32_t value) { return __builtin_clz(value); }
 int CountZeroL(uint64_t value) { return __builtin_clzll(value); }
@@ -142,6 +153,10 @@ int CountZeroR(uint64_t value) { return __builtin_ctzll(value); }
 #elif defined(_MSC_VER)
 // Microsoft Visual Studio.
 
+int CountBit(uint16_t value) { return int(__popcnt16(value)); }
+int CountBit(uint32_t value) { return int(__popcnt(value)); }
+int CountBit(uint64_t value) { return int(__popcnt64(value)); }
+
 int CountZeroL(uint16_t value) { return int(__lzcnt16(value)); }
 int CountZeroL(uint32_t value) { return int(__lzcnt(value)); }
 int CountZeroL(uint64_t value) { return int(__lzcnt64(value)); }
@@ -152,6 +167,10 @@ int CountZeroR(uint64_t value) { return int(_tzcnt_u64(value)); }
 
 #else
 // Fallback.
+
+int CountBit(uint16_t value) { return impl::CountBit16(value); }
+int CountBit(uint32_t value) { return impl::CountBit32(value); }
+int CountBit(uint64_t value) { return impl::CountBit64(value); }
 
 int CountZeroL(uint16_t value) { return impl::CountZeroL(value); }
 int CountZeroL(uint32_t value) { return impl::CountZeroL(value); }
