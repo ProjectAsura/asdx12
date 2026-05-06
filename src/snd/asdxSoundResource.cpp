fe2ce1ba05123f40666f26f35e15a9f3aecfe325
Pocol <project.asura@gmail.com>
2026-05-06 17:33:15 +0900

    フェード対応.

--- src/snd/asdxSoundResource.cpp
+++ src/snd/asdxSoundResource.cpp
@@ -4,17 +4,36 @@
 // Copyright(c) Project Asura. All right reserved.
 //-----------------------------------------------------------------------------
 
+#define XAUDIO2_HELPER_FUNCTIONS (1)
+
 //-----------------------------------------------------------------------------
 // Includes
 //-----------------------------------------------------------------------------
 #include <cassert>
 #include <fnd/asdxLogger.h>
+#include <fnd/asdxMath.h>
 #include <snd/asdxSoundResource.h>
 #include <snd/asdxSoundEngine.h>
 #include <xaudio2.h>
 
 
 namespace asdx {
+
+static_assert(sizeof (WaveFormat) == sizeof (WAVEFORMATEX), "WaveFormat Structure Size Not Matched.");
+static_assert(alignof(WaveFormat) == alignof(WAVEFORMATEX), "WaveFormat Structure Alignment Not Matched.");
+
+//-----------------------------------------------------------------------------
+//      デシベルからボリュームに変換します.
+//-----------------------------------------------------------------------------
+float DbToVolume(float db)
+{ return XAudio2DecibelsToAmplitudeRatio(db); }
+
+//-----------------------------------------------------------------------------
+//      ボリュームからデシベルに変換します.
+//-----------------------------------------------------------------------------
+float VolumeToDb(float volume)
+{ return XAudio2AmplitudeRatioToDecibels(volume); }
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // SoundResource class
@@ -35,7 +54,7 @@ SoundResource::~SoundResource()
 //-----------------------------------------------------------------------------
 //      初期化処理を行います.
 //-----------------------------------------------------------------------------
-bool SoundResource::Init(const SoundData& data)
+bool SoundResource::Init(const SoundData& data, const SoundBufferDesc& desc)
 {
     if (data.FormatData.empty() || data.AudioData.empty())
     {
@@ -59,10 +78,10 @@ bool SoundResource::Init(const SoundData& data)
         return false;
     }
 
-    m_AudioData = data.AudioData;
-    m_Loop      = data.Loop;
+    m_SounData = data;
+    m_Desc     = desc;
 
-    ResetBuffer();
+    ResetBuffer(desc);
 
     return true;
 }
@@ -72,16 +91,15 @@ bool SoundResource::Init(const SoundData& data)
 //-----------------------------------------------------------------------------
 void SoundResource::Term()
 {
-    m_AudioData.clear();
-    m_AudioData.shrink_to_fit();
+    m_SounData = {};
+    m_Desc     = {};
+    m_Loop     = false;
 
     if (m_pSourceVoice != nullptr)
     {
         m_pSourceVoice->DestroyVoice();
         m_pSourceVoice = nullptr;
     }
-
-    m_Loop = false;
 }
 
 //-----------------------------------------------------------------------------
@@ -91,11 +109,22 @@ void SoundResource::Play()
 {
     assert(m_pSourceVoice != nullptr);
     if (IsFinished())
-    { ResetBuffer(); }
+    { ResetBuffer(m_Desc); }
 
     auto hr = m_pSourceVoice->Start();
     if (FAILED(hr))
         ELOGA("Error : IXAudio2SourceVoice::Start() Failed. errcode = 0x%x", hr);
+}
+
+//-----------------------------------------------------------------------------
+//      一時停止処理を行います.
+//-----------------------------------------------------------------------------
+void SoundResource::Pause()
+{
+    assert(m_pSourceVoice != nullptr);
+    auto hr = m_pSourceVoice->Stop(XAUDIO2_PLAY_TAILS);
+    if (FAILED(hr))
+        ELOGA("Error : IXAudio2SourceVoice::Stop() Failed. errcode = 0x%x", hr);
 }
 
 //-----------------------------------------------------------------------------
@@ -107,6 +136,10 @@ void SoundResource::Stop()
     auto hr = m_pSourceVoice->Stop();
     if (FAILED(hr))
         ELOGA("Error : IXAudio2SourceVoice::Stop() Failed. errcode = 0x%x", hr);
+
+    hr = m_pSourceVoice->FlushSourceBuffers();
+    if (FAILED(hr))
+        ELOGA("Error : IXAudio2SourceVoice::FlushSourceBuffers() Failed. errcode = 0x%x", hr);
 }
 
 //-----------------------------------------------------------------------------
@@ -124,8 +157,6 @@ void SoundResource::ExitLoop()
     auto hr = m_pSourceVoice->ExitLoop();
     if (FAILED(hr))
         ELOGA("Error : IXAudio2SourceVoice::ExitLoop() Failed. errcode = 0x%x", hr);
-    else
-        m_Loop = false;
 }
 
 //-----------------------------------------------------------------------------
@@ -162,6 +193,26 @@ float SoundResource::GetVolume() const
 }
 
 //-----------------------------------------------------------------------------
+//      再生ピッチを設定します.
+//-----------------------------------------------------------------------------
+void SoundResource::SetPitch(float value)
+{
+    assert(m_pSourceVoice != nullptr);
+    m_pSourceVoice->SetFrequencyRatio(value);
+}
+
+//----------------------------------------------------------------------------
+//      再生ピッチを取得します.
+//----------------------------------------------------------------------------
+float SoundResource::GetPitch() const
+{
+    assert(m_pSourceVoice != nullptr);
+    float value = 0.0f;
+    m_pSourceVoice->GetFrequencyRatio(&value);
+    return value;
+}
+
+//-----------------------------------------------------------------------------
 //      ソースボイスを取得します.
 //-----------------------------------------------------------------------------
 IXAudio2SourceVoice* SoundResource::GetSourceVoice() const
@@ -170,15 +221,74 @@ IXAudio2SourceVoice* SoundResource::GetSourceVoice() const
 //-----------------------------------------------------------------------------
 //      バッファを再設定します.
 //-----------------------------------------------------------------------------
-void SoundResource::ResetBuffer()
+void SoundResource::ResetBuffer(const SoundBufferDesc& desc)
 {
     XAUDIO2_BUFFER buf = {};
-    buf.pAudioData = m_AudioData.data();
-    buf.AudioBytes = UINT32(m_AudioData.size());
-    buf.LoopCount  = (m_Loop) ? XAUDIO2_LOOP_INFINITE : 0;
-    buf.Flags      = (m_Loop) ? 0 : XAUDIO2_END_OF_STREAM;
+    buf.pAudioData = desc.pAudioData;
+    buf.AudioBytes = desc.AudioBytes;
+    buf.PlayBegin  = desc.PlayBegin;
+    buf.PlayLength = desc.PlayLength;
+    buf.LoopBegin  = desc.LoopBegin;
+    buf.LoopLength = desc.LoopLength;
+    buf.LoopCount  = desc.LoopCount;
+    buf.Flags      = desc.Flags;
+
+    m_Loop = (desc.LoopCount == XAUDIO2_LOOP_INFINITE);
+    m_Desc = desc;
 
     m_pSourceVoice->SubmitSourceBuffer(&buf);
+}
+
+//-----------------------------------------------------------------------------
+//      オーディオデータを取得します.
+//-----------------------------------------------------------------------------
+const std::vector<uint8_t>& SoundResource::GetAudioData() const
+{ return m_SounData.AudioData; }
+
+//-----------------------------------------------------------------------------
+//      波形フォーマットを取得します.
+//-----------------------------------------------------------------------------
+const WaveFormat* SoundResource::GetWaveFormat() const
+{ return reinterpret_cast<const WaveFormat*>(m_SounData.FormatData.data()); }
+
+//-----------------------------------------------------------------------------
+//      サウンドバッファ設定を取得します.
+//-----------------------------------------------------------------------------
+const SoundBufferDesc& SoundResource::GetDesc() const
+{ return m_Desc; }
+
+//-----------------------------------------------------------------------------
+//      音量フェード処理を行います.
+//-----------------------------------------------------------------------------
+void SoundResource::Fade(float targetVolume, float targetSec)
+{
+    m_FadeTargetVolume = targetVolume;
+    m_FadeStartVolume  = GetVolume();
+    m_FadeSec          = targetSec;
+    m_ElapsedSec       = 0.0f;
+}
+
+//-----------------------------------------------------------------------------
+//      更新処理を行います.
+//-----------------------------------------------------------------------------
+void SoundResource::Update(float deltaSec)
+{
+    if (m_FadeSec <= 0.0f)
+        return;
+
+    m_ElapsedSec += deltaSec;
+    auto complete = false;
+    if (m_ElapsedSec > m_FadeSec)
+    {
+        m_ElapsedSec = m_FadeSec;
+        complete     = true;
+    }
+
+    auto volume = Lerp(m_FadeStartVolume, m_FadeTargetVolume, Saturate(m_ElapsedSec / m_FadeSec));
+    SetVolume(volume);
+
+    if (complete)
+    { m_FadeSec = 0.0f; }
 }
 
 } // namespace asdx
