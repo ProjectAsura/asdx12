cb98bb70ca61ab65fd4e2cca79bb9872ad844a59
Pocol <project.asura@gmail>
2026-07-05 15:58:54 +0900

    コード整理.

--- res/shaders/asdxMoviePS.hlsl
+++ res/shaders/asdxMoviePS.hlsl
@@ -21,34 +21,22 @@ Texture2D    ColorMapY  : register(t0);
 Texture2D    ColorMapUV : register(t1);
 SamplerState ColorSmp   : register(s0);
 
-//-----------------------------------------------------------------------------
-//      SRGBからリニアへの変換.
-//-----------------------------------------------------------------------------
-float3 SRGB_To_Linear(float3 color)
-{
-    float3 result;
-    result.x = (color.x < 0.0405f) ? color.x / 12.92f : pow((abs(color.x) + 0.055) / 1.055f, 2.4f);
-    result.y = (color.y < 0.0405f) ? color.y / 12.92f : pow((abs(color.y) + 0.055) / 1.055f, 2.4f);
-    result.z = (color.z < 0.0405f) ? color.z / 12.92f : pow((abs(color.z) + 0.055) / 1.055f, 2.4f);
-
-    return result;
-}
 
 //-----------------------------------------------------------------------------
-//      YPbPr から RGB に変換します.
+//      YCbCr から RGB に変換します.
 //-----------------------------------------------------------------------------
-float3 YPbPrToRGB(float3 color)
+float3 YCbCrToRGB(float3 color)
 {
     // 正規化.
-    float Y  = (color.x * 255.0f - 16.0f) / 219.0f;
-    float Pb = (color.y * 255.0f - 128.0f) / 224.0f;
-    float Pr = (color.z * 255.0f - 128.0f) / 224.0f;
+    float Y  = (color.x * 255.0f -  16.0f) / 219.0f;
+    float Cb = (color.y * 255.0f - 128.0f) / 224.0f;
+    float Cr = (color.z * 255.0f - 128.0f) / 224.0f;
 
     // BT.709
     return float3(
-        Y + 1.5748f * Pr,
-        Y - 0.1873f * Pb - 0.4681f * Pr,
-        Y + 1.8556f * Pb);
+        Y + 1.5748f * Cr,
+        Y - 0.1873f * Cb - 0.4681f * Cr,
+        Y + 1.8556f * Cb);
 }
 
 //-----------------------------------------------------------------------------
@@ -56,15 +44,8 @@ float3 YPbPrToRGB(float3 color)
 //-----------------------------------------------------------------------------
 float4 main(const VSOutput iput) : SV_TARGET
 {
-    float  y  = ColorMapY .Sample(ColorSmp, iput.TexCoord).r;
-    float2 uv = ColorMapUV.Sample(ColorSmp, iput.TexCoord).rg;
- 
-    float4 result = 0.0f.xxxx;
-    result.rgb = YPbPrToRGB(float3(y, uv));
-#if 0
-    result.rgb = SRGB_To_Linear(result.rgb);
-#endif
-    result.a = 1.0f;
-
-    return result;
+    // YUV 形式から RGB 形式に変換して出力.
+    float  y  = ColorMapY .SampleLevel(ColorSmp, iput.TexCoord, 0.0f).r;
+    float2 uv = ColorMapUV.SampleLevel(ColorSmp, iput.TexCoord, 0.0f).rg;
+    return float4(YCbCrToRGB(float3(y, uv)), 1.0f);
 }
