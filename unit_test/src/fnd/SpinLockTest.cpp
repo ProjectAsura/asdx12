899566876b0dadf408328b2e0e8c445ae79fffea
Pocol <project.asura@gmail>
2026-09-01 22:31:03 +0900

    Read-Write SpinLockを追加.

--- unit_test/src/fnd/SpinLockTest.cpp
+++ unit_test/src/fnd/SpinLockTest.cpp
@@ -8,6 +8,7 @@
 // Includes
 //-----------------------------------------------------------------------------
 #include <fnd/asdxSpinLock.h>
+#include <atomic>
 #include <thread>
 
 
@@ -23,7 +24,6 @@ TEST(SpinLockTest, Basic)
     EXPECT_TRUE(lock.try_lock());
     lock.unlock();
 }
-
 TEST(SpinLockTest, MutualExclusion)
 {
     asdx::SpinLock lock;
@@ -80,3 +80,84 @@ TEST(SpinLockTest, RecursiveSpinLock)
     lock.unlock();
 }
 
+TEST(SpinLockTest, RwSpinLock)
+{
+    asdx::RwSpinLock<int> lock;
+
+    EXPECT_TRUE(lock.try_read_lock());
+    EXPECT_TRUE(lock.try_read_lock());
+    EXPECT_FALSE(lock.try_write_lock());
+
+    lock.read_unlock();
+    lock.read_unlock();
+
+    EXPECT_TRUE(lock.try_write_lock());
+    EXPECT_FALSE(lock.try_read_lock());
+    EXPECT_FALSE(lock.try_write_lock());
+    lock.write_unlock();
+
+    EXPECT_TRUE(lock.try_read_lock());
+    lock.read_unlock();
+}
+
+TEST(SpinLockTest, RwSpinLockAllowsConcurrentReaders)
+{
+    asdx::RwSpinLock<int> lock;
+    std::atomic<int> readers = 0;
+    std::atomic<bool> start = false;
+    std::thread threads[2];
+
+    for (auto& thread : threads)
+    {
+        thread = std::thread([&]() {
+            lock.read_lock();
+            ++readers;
+
+            while (!start.load())
+            { std::this_thread::yield(); }
+
+            lock.read_unlock();
+        });
+    }
+
+    while (readers.load() != 2)
+    { std::this_thread::yield(); }
+
+    EXPECT_FALSE(lock.try_write_lock());
+    start.store(true);
+
+    for (auto& thread : threads)
+    { thread.join(); }
+
+    EXPECT_TRUE(lock.try_write_lock());
+    lock.write_unlock();
+}
+
+TEST(SpinLockTest, ScopedReadLock)
+{
+    asdx::RwSpinLock<int> lock;
+
+    {
+        asdx::ScopedReadLock<asdx::RwSpinLock<int>> scopedLock(lock);
+        EXPECT_TRUE(lock.try_read_lock());
+        lock.read_unlock();
+        EXPECT_FALSE(lock.try_write_lock());
+    }
+
+    EXPECT_TRUE(lock.try_write_lock());
+    lock.write_unlock();
+}
+
+TEST(SpinLockTest, ScopedWriteLock)
+{
+    asdx::RwSpinLock<int> lock;
+
+    {
+        asdx::ScopedWriteLock<asdx::RwSpinLock<int>> scopedLock(lock);
+        EXPECT_FALSE(lock.try_read_lock());
+        EXPECT_FALSE(lock.try_write_lock());
+    }
+
+    EXPECT_TRUE(lock.try_read_lock());
+    lock.read_unlock();
+}
